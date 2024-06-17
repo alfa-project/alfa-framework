a1fdba819165ad852afae60c75f679380f1c5cb2
RijoRoriz <ricardojrroriz@gmail.com>
2024-06-17 15:54:26 +0100

    fix the path of hardware extensions and libs to be included in the IP rep

--- platforms/xilinx/tcl/zcu104/build.tcl
+++ platforms/xilinx/tcl/zcu104/build.tcl
@@ -57,7 +57,7 @@ if {[string equal [get_filesets -quiet sources_1] ""]} {
 # Set IP repository paths
 set obj [get_filesets sources_1]
 if { $obj != {} } {
-   set_property "ip_repo_paths" "[file normalize "$origin_dir/../../../../alfa-extensions/hw/"] [file normalize "$origin_dir/../../"]" $obj
+   set_property "ip_repo_paths" "[file normalize "$origin_dir/../../../../extensions/hw/"] [file normalize "$origin_dir/../../"] [file normalize "$origin_dir/../../../../libs/hw/"]" $obj
 
    # Rebuild user ip_repo's index before adding any source files
    update_ip_catalog -rebuild
