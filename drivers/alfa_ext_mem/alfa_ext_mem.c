aad9ac57b8d21f064deb0f033fa73acab3c73378
RijoRoriz <ricardojrroriz@gmail.com>
2024-07-10 14:47:05 +0100

    fix driver class name and function names

--- drivers/alfa_ext_mem/alfa_ext_mem.c
+++ drivers/alfa_ext_mem/alfa_ext_mem.c
@@ -31,13 +31,13 @@
 
 #define ALFA_EXT_MEM_GET_PHYS_ADDR _IOR('a', 1, unsigned long)
 
-static struct class *alfa_ext_class;
+static struct class *alfa_ext_mem_class;
 static struct platform_device *g_pdev = NULL;
 static void *dma_buf;
 static dma_addr_t dma_handle;
 static bool file_open = false;
 
-static int alfa_ext_open(struct inode *inode, struct file *filp) {
+static int alfa_ext_mem_open(struct inode *inode, struct file *filp) {
   if (file_open) {
     pr_err(
         "alfa_ext_mem: Failed to open /dev/alfa_ext_mem because it is already "
@@ -50,7 +50,7 @@ static int alfa_ext_open(struct inode *inode, struct file *filp) {
   return 0;
 }
 
-static int alfa_ext_release(struct inode *inode, struct file *filp) {
+static int alfa_ext_mem_release(struct inode *inode, struct file *filp) {
   if (!file_open) {
     pr_err(
         "alfa_ext_mem: Failed to release /dev/alfa_ext_mem because it is not "
@@ -62,8 +62,9 @@ static int alfa_ext_release(struct inode *inode, struct file *filp) {
   return 0;
 }
 
-static long alfa_ext_ioctl(struct file *file, unsigned int cmd,
+static long alfa_ext_mem_ioctl(struct file *file, unsigned int cmd,
                            unsigned long arg) {
+  printk("alfa_ext_mem: ioctl cmd=%d\n", cmd);
   switch (cmd) {
     case ALFA_EXT_MEM_GET_PHYS_ADDR:
       if (copy_to_user((void __user *)arg, &dma_handle, sizeof(dma_handle))) {
@@ -76,9 +77,11 @@ static long alfa_ext_ioctl(struct file *file, unsigned int cmd,
   return 0;
 }
 
-static int alfa_ext_mmap(struct file *filp, struct vm_area_struct *vma) {
+static int alfa_ext_mem_mmap(struct file *filp, struct vm_area_struct *vma) {
   unsigned long size = vma->vm_end - vma->vm_start;
   unsigned long off = vma->vm_pgoff << PAGE_SHIFT;
+
+  printk("alfa_ext_mem: mmap size=%lx, off=%lx\n", size, off);
 
   if (size > DMA_BUF_SIZE) {
     pr_err("alfa_ext_mem: Invalid mmap size: %lx (max: %lx)\n", size,
@@ -100,15 +103,15 @@ static int alfa_ext_mmap(struct file *filp, struct vm_area_struct *vma) {
   return 0;
 }
 
-static const struct file_operations alfa_ext_fops = {
+static const struct file_operations alfa_ext_mem_fops = {
     .owner = THIS_MODULE,
-    .open = alfa_ext_open,
-    .release = alfa_ext_release,
-    .unlocked_ioctl = alfa_ext_ioctl,
-    .mmap = alfa_ext_mmap,
+    .open = alfa_ext_mem_open,
+    .release = alfa_ext_mem_release,
+    .unlocked_ioctl = alfa_ext_mem_ioctl,
+    .mmap = alfa_ext_mem_mmap,
 };
 
-static int alfa_ext_probe(struct platform_device *pdev) {
+static int alfa_ext_mem_probe(struct platform_device *pdev) {
   int ret;
 
   g_pdev = pdev;
@@ -127,69 +130,69 @@ static int alfa_ext_probe(struct platform_device *pdev) {
       dma_buf, &dma_handle);
 
   // Create device class
-  if (!alfa_ext_class) {
-    alfa_ext_class = class_create(THIS_MODULE, CLASS_NAME);
-    if (IS_ERR(alfa_ext_class)) {
+  if (!alfa_ext_mem_class) {
+    alfa_ext_mem_class = class_create(THIS_MODULE, CLASS_NAME);
+    if (IS_ERR(alfa_ext_mem_class)) {
       pr_err("alfa_ext_mem: Failed to create device class");
-      ret = PTR_ERR(alfa_ext_class);
+      ret = PTR_ERR(alfa_ext_mem_class);
       dma_free_coherent(&pdev->dev, DMA_BUF_SIZE, dma_buf, dma_handle);
       return ret;
     }
   }
 
   // Create a character device to access the mapped memory
-  ret = register_chrdev(0, DEVICE_NAME, &alfa_ext_fops);
+  ret = register_chrdev(0, DEVICE_NAME, &alfa_ext_mem_fops);
   if (ret < 0) {
     pr_err("alfa_ext_mem: Failed to register character device\n");
-    if (alfa_ext_class) {
-      class_destroy(alfa_ext_class);
+    if (alfa_ext_mem_class) {
+      class_destroy(alfa_ext_mem_class);
     }
     dma_free_coherent(&pdev->dev, DMA_BUF_SIZE, dma_buf, dma_handle);
     return ret;
   }
 
-  device_create(alfa_ext_class, NULL, MKDEV(ret, 0), NULL, DEVICE_NAME);
+  device_create(alfa_ext_mem_class, NULL, MKDEV(ret, 0), NULL, DEVICE_NAME);
   pr_info("alfa_ext_mem: ALFA_EXT_MEM probed successfully\n");
   return 0;
 }
 
-static int alfa_ext_remove(struct platform_device *pdev) {
+static int alfa_ext_mem_remove(struct platform_device *pdev) {
   unregister_chrdev(0, DEVICE_NAME);
   pr_info("alfa_ext_mem: Unmapped physical memory\n");
-  if (alfa_ext_class) {
-    class_destroy(alfa_ext_class);
-    alfa_ext_class = NULL;
+  if (alfa_ext_mem_class) {
+    class_destroy(alfa_ext_mem_class);
+    alfa_ext_mem_class = NULL;
   }
   dma_free_coherent(&pdev->dev, DMA_BUF_SIZE, dma_buf, dma_handle);
   g_pdev = NULL;
   return 0;
 }
 
-static const struct of_device_id alfa_ext_of_ids[] = {
+static const struct of_device_id alfa_ext_mem_of_ids[] = {
     {.compatible = "alfa_ext_mem"}, {}};
-MODULE_DEVICE_TABLE(of, alfa_ext_of_ids);
+MODULE_DEVICE_TABLE(of, alfa_ext_mem_of_ids);
 
-static struct platform_driver alfa_ext_driver = {
-    .probe = alfa_ext_probe,
-    .remove = alfa_ext_remove,
+static struct platform_driver alfa_ext_mem_driver = {
+    .probe = alfa_ext_mem_probe,
+    .remove = alfa_ext_mem_remove,
     .driver =
         {
             .name = "alfa_ext_mem",
             .owner = THIS_MODULE,
-            .of_match_table = alfa_ext_of_ids,
+            .of_match_table = alfa_ext_mem_of_ids,
         },
 };
 
-static int __init alfa_ext_init(void) {
-  return platform_driver_register(&alfa_ext_driver);
+static int __init alfa_ext_mem_init(void) {
+  return platform_driver_register(&alfa_ext_mem_driver);
 }
 
-static void __exit alfa_ext_exit(void) {
-  platform_driver_unregister(&alfa_ext_driver);
+static void __exit alfa_ext_mem_exit(void) {
+  platform_driver_unregister(&alfa_ext_mem_driver);
 }
 
-module_init(alfa_ext_init);
-module_exit(alfa_ext_exit);
+module_init(alfa_ext_mem_init);
+module_exit(alfa_ext_mem_exit);
 
 MODULE_LICENSE("GPL");
 MODULE_AUTHOR("Ricardo Roriz");
