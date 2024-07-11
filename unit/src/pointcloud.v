e1a05fedab8f8129674d9e19bc6d76b6e0f9cb57
RijoRoriz <ricardojrroriz@gmail.com>
2024-07-11 01:09:39 +0100

    fix bit width for 64 bit addressing

--- unit/src/pointcloud.v
+++ unit/src/pointcloud.v
@@ -109,17 +109,17 @@ module MemMU_pointcloud #(
       wire [31:0] readAddress_high_bits;
       wire [31:0] rewriteAddress_high_bits;
 
-      assign writeAddress_high_bits[3:0] = 3'b00;
-      assign writeAddress_high_bits[21:4] = representationAddress;
-      assign writeAddress_high_bits[31:22] = 0;
+      assign writeAddress_high_bits[2:0] = 3'b00;
+      assign writeAddress_high_bits[20:3] = representationAddress;
+      assign writeAddress_high_bits[31:21] = 0;
 
-      assign readAddress_high_bits[3:0] = 3'b00;
-      assign readAddress_high_bits[21:4] = i_ExMU_pointReadID;
-      assign readAddress_high_bits[31:22] = 0;
+      assign readAddress_high_bits[2:0] = 3'b00;
+      assign readAddress_high_bits[20:3] = i_ExMU_pointReadID;
+      assign readAddress_high_bits[31:21] = 0;
 
-      assign rewriteAddress_high_bits[3:0] = 3'b00;
-      assign rewriteAddress_high_bits[21:4] = i_ExMU_pointReWriteID;
-      assign rewriteAddress_high_bits[31:22] = 0;
+      assign rewriteAddress_high_bits[2:0] = 3'b00;
+      assign rewriteAddress_high_bits[20:3] = i_ExMU_pointReWriteID;
+      assign rewriteAddress_high_bits[31:21] = 0;
 
 
       assign o_MemMU_P_writeAddress = writeAddress_high_bits + i_MonU_MemMU_parameter;
