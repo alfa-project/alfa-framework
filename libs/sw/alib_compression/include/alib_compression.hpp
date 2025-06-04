a71ae7462371fe4b1388022045888146e6d80873
RijoRoriz <ricardojrroriz@gmail.com>
2025-06-04 13:33:24 +0100

    fix format

--- libs/sw/alib_compression/include/alib_compression.hpp
+++ libs/sw/alib_compression/include/alib_compression.hpp
@@ -1,6 +1,6 @@
 
 /*
- * Copyright 2023 ALFA Project. All rights reserved.
+ * Copyright 2025 ALFA Project. All rights reserved.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
